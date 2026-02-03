daaab653219f8c3a889de3dd1dd260da337ad88a
websiterdevelopper <aziz04.ben@gmail.com>
2026-02-03 18:54:20 +0100

    Added hamburger menu click and animation

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,17 @@
 let hamburgerMenu = document.querySelector(".ham-menu");
 let navList = document.querySelector("nav ul");
+
+// Click animation
+if (document.body.contains(hamburgerMenu) && navList.classList.length == 1) {
+  navList.classList.add("hammenu-out-animation");
+}
+
+hamburgerMenu.addEventListener("click", () => {
+  if (navList.classList.contains("hammenu-out-animation")) {
+    navList.classList.add("hammenu-in-animation");
+    navList.classList.remove("hammenu-out-animation");
+  } else {
+    navList.classList.add("hammenu-out-animation");
+    navList.classList.remove("hammenu-in-animation");
+  }
+});
